b21588dddc6bfb767568ef2b6cf67d4c2bd7d3b3
Alexsandro Martins Alves <alexsandromartins.0172@gmail.com>
2023-12-10 17:08:48 -0300

    add instituicaomodel

--- gpip/src/main/controller/InstituicaoController.java
+++ gpip/src/main/controller/InstituicaoController.java
@@ -1,5 +1,23 @@
 package main.controller;
 
+import main.entity.Instituicao;
+import main.entity.Localizacao;
+import main.entity.Usuario;
+import main.model.InstituicaoModel;
+
 public class InstituicaoController {
-    
+
+    private Instituicao instituicao;
+
+    public InstituicaoController() {
+       }
+
+       public void CadastrarInstituicao(String nome, Localizacao localizacao) {
+        InstituicaoModel instituicaoModel = new InstituicaoModel(nome, localizacao);
+        if (instituicaoModel.CadastroInstituicao()) {
+            System.out.println("Instituição Cadastrada com sucesso!");
+        }else {
+            System.out.println("Instituição com a mesma localização já existe.");
+        }
+    }
 }
